def62c6a11e9b1a67fdcbdf60a19638fb6389044
calebbabin <object.caleb@gmail.com>
2022-09-02 14:54:56 -0500

    center navbar items

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -13,7 +13,7 @@ const NavItem = ({ children, href }) => {
 const Navbar = () => {
 	return (
 		<div className="relative z-50 text-white">
-			<nav className="max-w-7xl mx-auto w-11/12 px-2 py-4 box-border flex justify-between">
+			<nav className="max-w-7xl mx-auto w-11/12 px-2 py-4 box-border flex justify-between items-center">
 				<h2 className="my-0 font-semibold text-white">
 					<SmartLink href="/">
 						<Image src="/img/enterprise.svg" width={165} height={15} alt="" />
