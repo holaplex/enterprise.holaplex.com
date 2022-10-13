ef174ebe3a73af1111abf8df1098c7fc22eef5c4
Kristian Elset Boe <kristian.e.boe@gmail.com>
2022-10-13 19:30:59 +0200

    chore: rewrite

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,8 @@ export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
     splitbee.init({
       token: process.env.NEXT_PUBLIC_SPLITBEE_TOKEN,
-      scriptUrl: "/bee.js", // disabled until I figure out redirects
-      apiUrl: "/_hive",
+      // scriptUrl: "/bee.js", // disabled until I figure out redirects
+      // apiUrl: "/_hive",
     });
   }, []);
 
