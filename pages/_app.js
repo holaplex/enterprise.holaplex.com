4028ef59d805bf4449da4f93fb6de358ee768ef6
Kristian Elset Boe <kristian.e.boe@gmail.com>
2022-10-13 15:55:30 +0200

    fix: removed splitbee redirects

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,13 @@ import splitbee from "@splitbee/web";
 import { useEffect } from "react";
 import "../styles/fonts.scss";
 import "../styles/global.scss";
+
 export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
     splitbee.init({
       token: process.env.NEXT_PUBLIC_SPLITBEE_TOKEN,
-      scriptUrl: "/bee.js",
-      apiUrl: "/_hive",
+      // scriptUrl: "/bee.js", // disabled until I figure out redirects
+      // apiUrl: "/_hive",
     });
   }, []);
 
